55936d3ca4d307b581feafec10b21a4fa34abe1e
biswasbittu <biswasbittu7415@yahoo.com>
2024-04-20 17:59:23 +0530

    login page done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 
-import {  NavLink } from 'react-router-dom';
+import {  Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
 
@@ -9,15 +9,15 @@ const Header = () => {
     <div className=" bg-slate-500 p-3">
       <div className="container mx-auto">
         <nav className="flex items-center justify-between">
-          <div>
+          <Link to='/'>
             <div className="avatar">
               <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                 <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
               </div>
             </div>
-          </div>
+          </Link>
           <div>
-            <ul className="flex gap-5 text-sky-400 text-lg font-bold">
+            <ul className="flex gap-5 text-sky-300 text-lg font-bold">
               <NavLink
                 to="/"
                 className={({isActive}) => (isActive ? "text-blue-700" : "")}
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,78 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
-        <div>
-            <h1>Simple Login page</h1>
+      <div>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+          <div className="bg-white p-8 rounded-lg shadow-lg md:flex">
+            {/* Login Form */}
+            <div className="md:w-1/2">
+              <h2 className="text-2xl font-bold mb-4">Login</h2>
+              <form className="space-y-4">
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Name
+                  </label>
+                  <input
+                    type="name"
+                    name="name"
+                    className="mt-1 p-2 w-full border rounded-md"
+                    placeholder="Enter your Name"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="mt-1 p-2 w-full border rounded-md"
+                    placeholder="Enter your email"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="mt-1 p-2 w-full border rounded-md"
+                    placeholder="Enter your password"
+                    required
+                  />
+                </div>
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full bg-sky-500  p-2 rounded-md hover:bg-sky-600 "
+                  >
+                    Login
+                  </button>
+                </div>
+                <p className="text-center">
+                  are you new? <Link to="/register" >register here..</Link>
+                </p>
+              </form>
+            </div>
+
+            {/* Image */}
+            <div className="hidden md:block md:w-1/2">
+              <img
+                src="../../../public/log-img.png"
+                alt="Login"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 

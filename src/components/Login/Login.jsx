3cbbda2313f3d092cc12dfe063845b2296aa339d
biswasbittu <biswasbittu7415@yahoo.com>
2024-04-25 17:21:12 +0530

    login done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,92 +1,135 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
-
+import app from "../Firebase/firebase.config";
+import { useState } from "react";
 
 const Login = () => {
+  const auth = getAuth(app);
 
-  const handllogIn =( e )=>{
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handllogIn = (e) => {
     e.preventDefault();
 
-   const email = e.target.email.value;
-   const password = e.target.password.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
 
-   console.log(email,password)
-    
-    
-  }
+    console.log(email, password);
+    setSuccess("");
+    setError("");
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setSuccess("Log in Succeffully");
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        //  console.log(errorMsg);
+        setError(errorMsg);
+      });
+  };
 
-  
-    return (
-      <div>
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-          <div className="bg-white p-8 rounded-lg shadow-lg md:flex">
-            {/* Login Form */}
-            <div className="md:w-1/2">
-              <h2 className="text-2xl font-bold mb-4">Login</h2>
-              <form className="space-y-4" onSubmit={handllogIn}>
-                {/* <div>
-                  <label className="block text-sm font-medium text-gray-600">
-                    Name
-                  </label>
-                  <input
-                    type="name"
-                    name="name"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    placeholder="Enter your Name"
-                    required
-                  />
-                </div> */}
-
-                <div>
-                  <label className="block text-sm font-medium text-gray-600">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    placeholder="Enter your email"
-                    required
-                  />
-                </div>
-
-                <div>
-                  <label className="block text-sm font-medium text-gray-600">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    placeholder="Enter your password"
-                    required
-                  />
-                </div>
-                <div>
-                  <button
-                    type="submit"
-                    className="w-full bg-sky-500  p-2 rounded-md hover:bg-sky-600 "
-                  >
-                    Login
-                  </button>
-                </div>
-                <p className="text-center text-sky-400">
-                  are you new? <Link to="/register">register here..</Link>
-                </p>
-              </form>
-            </div>
-
-            {/* Image */}
-            <div className="hidden md:block md:w-1/2">
-              <img
-                src="../../../public/log-img.png"
-                alt="Login"
-                className="w-full h-full object-cover rounded-lg"
+  return (
+    <div>
+      {success && (
+        <div className="container mx-auto w-1/3">
+          <div role="alert" className="alert alert-success">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
               />
-            </div>
+            </svg>
+            <span>{success}</span>
+          </div>
+        </div>
+      )}
+      {error && (
+        <div className="container mx-auto w-1/3">
+          <div role="alert" className="alert alert-error">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>{error}</span>
+          </div>
+        </div>
+      )}
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg md:flex">
+          {/* Login Form */}
+          <div className="md:w-1/2">
+            <h2 className="text-2xl font-bold mb-4">Login</h2>
+            <form className="space-y-4" onSubmit={handllogIn}>
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  className="mt-1 p-2 w-full border rounded-md"
+                  placeholder="Enter your email"
+                  required
+                />
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  className="mt-1 p-2 w-full border rounded-md"
+                  placeholder="Enter your password"
+                  required
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-sky-500  p-2 rounded-md hover:bg-sky-600 "
+                >
+                  Login
+                </button>
+              </div>
+              <p className="text-center text-sky-400">
+                are you new? <Link to="/register">register here..</Link>
+              </p>
+            </form>
+          </div>
+
+          {/* Image */}
+          <div className="hidden md:block md:w-1/2">
+            <img
+              src="../../../public/log-img.png"
+              alt="Login"
+              className="w-full h-full object-cover rounded-lg"
+            />
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
